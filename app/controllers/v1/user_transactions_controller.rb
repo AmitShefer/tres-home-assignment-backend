bcd0ed087c42a2c502ae2e2de15159f6af73235b
Amit Shefer <amshefer@gmail.com>
2023-09-10 12:58:40 +0300

    temp

--- app/controllers/v1/user_transactions_controller.rb
+++ app/controllers/v1/user_transactions_controller.rb
@@ -7,9 +7,9 @@ class V1::UserTransactionsController < ApplicationController
     start_date = params[:start_date]
     end_date = params[:end_date]
 
-    cache_key = "user_transactions/#{user_id}/#{start_value}/#{end_value}/#{start_date}/#{end_date}"
+    cache_key = "user_transactions/u#{user_id}/#{start_value}/#{end_value}/#{start_date}/#{end_date}"
 
-    transactions = Rails.cache.fetch(cache_key, expires_in: 30.seconds) do
+    transactions = Rails.cache.fetch(cache_key, expires_in: 30.minutes) do
       UserTransaction.where(user_id: user_id,
                             value: start_value..end_value,
                             date: start_date..end_date).to_a
